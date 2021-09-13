3bdef9807e394e3b3e84ab04761ba7ebbf0fcb90
dantes47 <dantes817@gmail.com>
2021-09-13 18:13:32 +0300

    Initial commit/ setting-up new project/mysql db

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# class Demo Controller
+class DemoController < ApplicationController
+  layout false
+
+  def index; end
+end
---
+++ test/controllers/demo_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DemoControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get demo_index_url
+    assert_response :success
+  end
+end
